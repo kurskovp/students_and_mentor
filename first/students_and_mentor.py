f07900a1a79e8d7a2dd953fb4ec1cb34a09f6879
Your Name <kurskovp@gmail.com>
2021-02-25 03:43:50 +0300

    Добавлен родительский класс Mentor и два наследных класса Lecturer и Reviewer

--- first/students_and_mentor.py
+++ first/students_and_mentor.py
@@ -6,3 +6,26 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+
+class Mentor:
+    name = ''
+    surname =''
+    teaching_course =[]
+
+    def __init__(self, name, surname,):
+        self.name = name
+        self.surname = surname
+        self.teaching_course = []
+
+
+
+class Lecturer(Mentor):
+    def __init__(self,name, surname, teaching_course):
+        Mentor. __init__ (self,name, surname, teaching_course)
+
+class Reviewer(Mentor):
+    def __init__(self,name, surname, teaching_course):
+        Mentor. __init__ (self,name, surname, teaching_course)
+
+
