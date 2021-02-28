467e28314da8a5029d4ea4a1246b914de09f9df0
Your Name <kurskovp@gmail.com>
2021-02-28 14:53:51 +0300

    Внесены следующие изменения и дополнения: 1. Поправлен проверочный блок для класса Students; 2. В класс Reviewer добавлена функция проверки домашних заданий по курсам у студентов и выставление им оценок; 3. Создан проверочный блок для класса Reviewer с проверкой одного студента.

--- first/students_and_mentor.py
+++ first/students_and_mentor.py
@@ -25,11 +25,25 @@ class Reviewer(Mentor):
     def __init__(self,name, surname, teaching_course):
         Mentor. __init__ (self,name, surname, teaching_course)
 
+    def set_student_grade(self, student, cours, grade):
+        if cours in student.courses_in_progress:
+            if cours in self.teaching_course:
+                student.grades[cours] = grade
+            else:
+                print(f'Преподаватель этот курс не должен проверять - {cours}')
+        else:
+            print(f'Студент не записан на данный курс - {cours}')
+
 
 
 # Проверочный блок:
 
 john = Student("John", "Smith", "male")
-john.courses_in_progress = ["Python", "Git", "Введение в програмирование"]
-
+john.courses_in_progress = ["Python", "Git"]
+print(john.name)
 print(john.courses_in_progress[1])
+
+rev_1 = Reviewer('Nic', 'Popov', ["Math", "Physics", "Python"])
+rev_1.set_student_grade(john, 'Math', 4)
+rev_1.set_student_grade(john, 'Python', 5)
+print(john.grades)
