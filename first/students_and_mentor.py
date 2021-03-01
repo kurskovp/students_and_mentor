354c0ce4fd358fb93b7e11353417681f3dfe7646
Your Name <kurskovp@gmail.com>
2021-03-01 12:22:22 +0300

    Внесены следующие изменения и дополнения: 1. В класса Studentsдобавлена функция выставления оценок лекторам; 2. В класс Ltcture добавлено получение оценок; 3. Добавлен проверочный блок для проверки выстановленных оценок для лекторов.

--- first/students_and_mentor.py
+++ first/students_and_mentor.py
@@ -6,6 +6,14 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def set_lecturer_grade(self, lecturer, course, grade):
+        if course in self.courses_in_progress:
+            if course in lecturer.teaching_course:
+                lecturer.grades[course] = grade
+            else:
+                print(f'Преподаватель этот курс не ведет - {course}')
+        else:
+            print(f'Студент не записан на данный курс, оценку лектору поставить не может - {course}')
 
 
 class Mentor:
@@ -19,6 +27,7 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self,name, surname, teaching_course):
         Mentor. __init__ (self,name, surname, teaching_course)
+        self.grades = {}
 
 
 class Reviewer(Mentor):
@@ -47,3 +56,10 @@ rev_1 = Reviewer('Nic', 'Popov', ["Math", "Physics", "Python"])
 rev_1.set_student_grade(john, 'Math', 4)
 rev_1.set_student_grade(john, 'Python', 5)
 print(john.grades)
+
+lec_1 = Lecturer("Adam", "Nolan", ["Biology", "Geom", "Python"])
+lec_2 = Lecturer("Игорь", "Петров", ["Math", "Git", "Physics"])
+john.set_lecturer_grade(lec_1, "Biology", 3)
+john.set_lecturer_grade(lec_1, "Geo", 4)
+john.set_lecturer_grade(lec_1, "Python", 5)
+print(lec_1.grades)
